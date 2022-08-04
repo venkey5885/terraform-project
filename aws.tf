216e562a3e399f6e1a88f903fd1c89f1dabd1538
venkey5885 <108710600+venkey5885@users.noreply.github.com>
2022-08-05 00:17:59 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "venkey-tera" {
+ami = "ami-068257025f72f470d"
+instance_type = "t2.micro"
+count = 1
+subnet_id = "subnet-0d47c7cc17ec8f644"
+
+tags = {
+Name = "venkey-tera"
+}
+}
